14d8e391915a2bea419ff98ee3aa3a1ec60ebd82
sivrish.t <sivrish.t@zohocorp.com>
2018-04-03 11:24:38 +0530

    Initial Commit

---
+++ MyTestProject/SegueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SegueViewController.swift
+//  MyTestProject
+//
+//  Created by Sivrish Thangamani on 03/04/18.
+//  Copyright © 2018 Sivrish Thangamani. All rights reserved.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
